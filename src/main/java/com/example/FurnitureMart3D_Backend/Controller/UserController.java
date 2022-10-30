c0d1482541b8451d20d112b1eb13bbc7296b700c
Heshan Nanayakkara <kalaruheshan@gmail.com>
2022-10-30 23:17:40 +0530

    Completed backend for User Registration

--- src/main/java/com/example/FurnitureMart3D_Backend/Controller/UserController.java
+++ src/main/java/com/example/FurnitureMart3D_Backend/Controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.FurnitureMart3D_Backend.Controller;
 
+import com.example.FurnitureMart3D_Backend.Dto.User.EmailDto;
+import com.example.FurnitureMart3D_Backend.Dto.User.PasswordDto;
+import com.example.FurnitureMart3D_Backend.Dto.User.UsernameDto;
 import com.example.FurnitureMart3D_Backend.Model.User;
 import com.example.FurnitureMart3D_Backend.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
@@ -24,9 +28,56 @@ public class UserController {
         return "Success";
     }
 
-
+    /*
     @GetMapping("/usersList")
     public List<User> getUsers() {
         return repository.findAll();
+    }*/
+
+    @GetMapping("/noOfUsers")
+    public int getUsers() {
+        return (int) repository.count();
+    }
+
+    @GetMapping( value = "/getAllUsernames" , produces = APPLICATION_JSON_VALUE)
+    public List<UsernameDto> findUserNames(){
+        List<UsernameDto> userNameList = new ArrayList<>();
+        try {
+            List<User> userList = repository.findAll();
+            for (User user: userList) {
+                userNameList.add(new UsernameDto(user.getUsername()));
+            }
+        }catch (Exception e){
+            System.out.println("Exception in find Usernames in User Controller");
+        }
+        return userNameList;
+    }
+
+    @GetMapping( value = "/getAllUserEmails" , produces = APPLICATION_JSON_VALUE)
+    public List<EmailDto> findUserEmails(){
+        List<EmailDto> userEmailList = new ArrayList<>();
+        try {
+            List<User> userList = repository.findAll();
+            for (User user: userList) {
+                userEmailList.add(new EmailDto(user.getEmail()));
+            }
+        }catch (Exception e){
+            System.out.println("Exception in find Emails in User Controller");
+        }
+        return userEmailList;
+    }
+
+    @GetMapping( value = "/getAllUserPasswords" , produces = APPLICATION_JSON_VALUE)
+    public List<PasswordDto> findUserPasswords(){
+        List<PasswordDto> userPasswordList = new ArrayList<>();
+        try {
+            List<User> userList = repository.findAll();
+            for (User user: userList) {
+                userPasswordList.add(new PasswordDto(user.getPassword()));
+            }
+        }catch (Exception e){
+            System.out.println("Exception in find Passwords in User Controller");
+        }
+        return userPasswordList;
     }
 }
---
+++ src/main/java/com/example/FurnitureMart3D_Backend/Dto/User/EmailDto.java
@@ -0,0 +1,10 @@
+package com.example.FurnitureMart3D_Backend.Dto.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class EmailDto {
+    private String email;
+}
